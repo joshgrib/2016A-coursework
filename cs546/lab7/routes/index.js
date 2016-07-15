806df006854d9a3c8549873546dd6af9d3c03cdd
joshgrib <jgribbon@stevens.edu>
2016-07-15 10:31:38 -0400

    Change path and add method to deliver css file

--- cs546/lab7/routes/index.js
+++ cs546/lab7/routes/index.js
@@ -15,12 +15,17 @@ const constructorMethod = (app) => {
     app.use(eventsPath, eventRoutes);
     app.use(locationsPath, locationRoutes);
 
+    app.use(settings.path+'/style', (req, res) => {
+        //send the css
+        res.sendFile('main.css', { root: path.join(__dirname, '../public/css') })
+    });
+
     app.use("*", (req, res) => {
         // any unmatched routes (ie, pages that do not exist) will hit this catch-all route
         res.status(404).render('404', {error: "Requested page not found"});
 
         // You could also do res.status(num).render(template, data)
-    })
+    });
 };
 
 module.exports = constructorMethod;
